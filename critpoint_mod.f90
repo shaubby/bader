10f4b95c3dcb289503c4cee2b2e491d246ec0661
shaubby <52765209+shaubby@users.noreply.github.com>
2025-07-14 14:12:06 -0500

    fix

--- critpoint_mod.f90
+++ critpoint_mod.f90
@@ -408,51 +408,8 @@
     END DO
   END SUBROUTINE GetCPCL
 
- SUBROUTINE SearchWithCPCLMultithread(bdr, chg, cpcl, cpl, cptnum, ucptnum, ucpCounts, opts)
-  USE omp_lib
-  TYPE(bader_obj) :: bdr
-  TYPE(charge_obj) :: chg
-  TYPE(cpc), ALLOCATABLE, DIMENSION(:) :: cpcl, cpl
-  TYPE(options_obj) :: opts
 
-  REAL(q2), DIMENSION(8,3,3) :: nnHes
-  REAL(q2), DIMENSION(3,3) :: interpolHessian
-  REAL(q2), DIMENSION(3) :: temcap, temscale, trueR, distance
-  REAL(q2) :: temnormcap
-
-  INTEGER, DIMENSION(4) :: ucpCounts
-  INTEGER, DIMENSION(2) :: connectedAtoms 
-  INTEGER :: i, cptnum, ucptnum
-
-  !$OMP PARALLEL DO PRIVATE(i, temcap, temscale, temnormcap, trueR, interpolHessian, connectedAtoms) &
-  !$OMP SHARED(cpcl, cpl, bdr, chg, opts, cptnum, ucpCounts, ucptnum) DEFAULT(SHARED)
-  DO i = 1, cptnum
-    cpcl(i)%isunique = .FALSE.
-    temcap = (/1.0_q2, 1.0_q2, 1.0_q2/)
-    temscale = (/1.0_q2, 1.0_q2, 1.0_q2/)
-    temnormcap = 1.0_q2
-
-    IF (opts%gradMode) THEN
-      CALL GradientDescend(bdr, chg, opts, trueR, cpcl(i)%ind, cpcl(i)%isUnique, 3000)
-    ELSE
-      CALL NRTFGP(bdr, chg, opts, trueR, cpcl(i)%isUnique, cpcl(i)%r, cpcl(i)%ind, 1000)
-      
-    END IF
-
-    IF (cpcl(i)%isUnique) THEN
-      cpcl(i)%trueind = trueR
-      interpolHessian = CDHessianR(trueR, chg)
-
-      !$OMP CRITICAL
-      ucptnum = ucptnum + 1
-      CALL RecordCPR(trueR, chg, cpl, ucptnum, connectedAtoms, ucpCounts, opts, interpolHessian, cpcl(i)%ind)
-      !$OMP END CRITICAL
-    END IF
-  END DO
-  !$OMP END PARALLEL DO
-END SUBROUTINE SearchWithCPCLMultithread
-
-SUBROUTINE SearchWithCPCL(bdr,chg,cpcl,cpl,cptnum,ucptnum,ucpCounts,opts)
+  SUBROUTINE SearchWithCPCL(bdr,chg,cpcl,cpl,cptnum,ucptnum,ucpCounts,opts)
     TYPE(bader_obj) :: bdr
     TYPE(charge_obj) :: chg
     TYPE(cpc), ALLOCATABLE, DIMENSION(:) :: cpcl,cpl
