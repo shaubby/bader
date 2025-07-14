bf04b0825cc373f1dfb36420e605c9e6f8237733
shaubby <52765209+shaubby@users.noreply.github.com>
2025-07-14 13:39:18 -0500

    fix

--- critpoint_mod.f90
+++ critpoint_mod.f90
@@ -457,9 +457,9 @@
 
         count_local(thread_id + 1) = count_local(thread_id + 1) + 1
         my_ucptnum = count_local(thread_id + 1)
-
-        CALL RecordCPR(trueR, chg, cpl_local(thread_id + 1, my_ucptnum), &
-                      0, connectedAtoms, ucpCounts, opts, interpolHessian, cpcl(i)%ind)
+        
+        CALL RecordCPR(trueR, chg, cpl_local(thread_id + 1, :), &
+              0, connectedAtoms, ucpCounts, opts, interpolHessian, cpcl(i)%ind)
       END IF
     END DO
     !$OMP END DO
