bd9dcd1401fa418ab24cde74d4ac690031b59834
shaubby <52765209+shaubby@users.noreply.github.com>
2025-07-14 13:23:52 -0500

    fix

--- critpoint_mod.f90
+++ critpoint_mod.f90
@@ -814,7 +814,7 @@ SUBROUTINE SearchWithCPCL(bdr,chg,cpcl,cpl,cptnum,ucptnum,ucpCounts,opts)
       ELSE 
         ! Loop through every grid point once and collect a list of points to start
         ! CP searching trajectories into cpcl, the CP candidate list.
-        CALL GetCPCL(bdr,chg,cpl,cpcl,opts,cptnum)
+        CALL GetCPCL_Spatial(bdr,chg,cpl,cpcl,opts,cptnum)
         IF (cptnum > 100000) THEN
           stat = 0
         ELSE 
