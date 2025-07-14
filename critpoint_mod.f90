65dc58134858eb187e156a46591257a085ba0327
shaubby <52765209+shaubby@users.noreply.github.com>
2025-07-14 14:12:38 -0500

    fix

--- critpoint_mod.f90
+++ critpoint_mod.f90
@@ -457,7 +457,53 @@
     END DO
   END SUBROUTINE SearchWithCPCL
 
+  SUBROUTINE SearchWithCPCLParallelized(bdr,chg,cpcl,cpl,cptnum,ucptnum,ucpCounts,opts)
+    TYPE(bader_obj) :: bdr
+    TYPE(charge_obj) :: chg
+    TYPE(cpc), ALLOCATABLE, DIMENSION(:) :: cpcl,cpl
+    
+    TYPE(options_obj) :: opts
 
+    REAL(q2), DIMENSION(8,3,3) :: nnHes
+    REAL(q2), DIMENSION(3,3) :: interpolHessian
+    REAL(q2),DIMENSION(3) :: temcap,temscale,trueR,distance
+    REAL(q2) :: temnormcap
+
+    INTEGER, DIMENSION(4) :: ucpCounts
+    INTEGER, DIMENSION(2) :: connectedAtoms
+    INTEGER :: i,cptnum,ucptnum       
+    DO i = 1, cptnum
+      cpcl(i)%isunique = .FALSE.
+      temcap = (/1.,1.,1./)
+      temscale = (/1.,1.,1./)
+      temnormcap = 1.
+      IF (opts%gradMode) THEN
+         !uses GradientDescend instead of NRTFGP          
+         CALL GradientDescend(bdr,chg,opts,trueR,cpcl(i)%ind,&
+         cpcl(i)%isUnique,3000)
+      ELSE
+         ! Begins newton raphson validation process
+         CALL NRTFGP(bdr,chg,opts,trueR,&
+         cpcl(i)%isUnique,cpcl(i)%r,cpcl(i)%ind,&
+         1000)
+      END IF
+      IF (cpcl(i)%isUnique ) THEN
+        !CALL MakeCPRoster(cpRoster,i,truer) !not sure what this does
+        cpcl(i)%trueind = trueR
+        interpolHessian = trilinear_interpol_hes(nnHes,distance)
+        ucptnum = ucptnum + 1
+        interpolHessian = CDHessianR(truer,chg)
+        CALL RecordCPR(truer,chg,cpl,ucptnum,connectedAtoms, ucpCounts, &
+          opts, interpolHessian, &
+          cpcl(i)%ind)
+        CYCLE
+      ELSE
+        CYCLE
+      END IF
+      CALL pbc_r_lat(trueR,chg%npts)
+    ! moving on to the next critical pint candidate
+    END DO
+  END SUBROUTINE SearchWithCPCLParallelized
 
   ! This subroutine reads in a list of CPs and their types, runs it through ReduceCP and PHRuleExam
   SUBROUTINE StaticCheck(bdr,chg,opts,ions)
@@ -786,7 +832,7 @@
           ! point is within half lattice to another, do not record this new point.
           ALLOCATE(cpRoster(cptnum,3))
           IF (LDM_Trajectories) ALLOCATE(fullcpRoster(cptnum,3))
-          CALL SearchWithCPCL(bdr,chg,cpcl,cpl,cptnum,ucptnum,ucpCounts,opts)
+          CALL SearchWithCPCLParallelized(bdr,chg,cpcl,cpl,cptnum,ucptnum,ucpCounts,opts)
 
           PRINT *, 'Number of critical point count: ', ucptnum
           PRINT *, 'Number of nuclear, bond, ring and cage  critical point &
